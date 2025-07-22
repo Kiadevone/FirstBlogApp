2c4a1c8a0e7dcc35d3e57e217b50b8c88af0ebb8
Kiarash Rahimi <xrx.mio33@gmail.com>
2025-07-22 20:52:32 +0330

    add analytic component

---
+++ src/app/pages/dashbord/components/AnalyticsButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AnalyticsButton = () => {
+  return (
+    <div>
+      <button className="px-8 py-4 text-white text-xl font-semibold bg-slate-500 rounded-md">
+        Analytics
+      </button>
+    </div>
+  );
+};
+
+export default AnalyticsButton;
