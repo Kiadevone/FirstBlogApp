bdb934e07365f601b371944b92686a4c7a2ca670
Kiarash Rahimi <xrx.mio33@gmail.com>
2025-07-22 20:53:17 +0330

    add NewPost Butto componentn

---
+++ src/app/pages/dashbord/components/NewPostButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NewPostButton = () => {
+  return (
+    <div>
+      <button className="px-8 py-4 text-white text-xl font-semibold bg-blue-500 rounded-md">
+        New Post
+      </button>
+    </div>
+  );
+};
+
+export default NewPostButton;
